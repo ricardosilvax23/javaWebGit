bb6dc2d9be43418bba19f9167366224e7ed56834
dark23 <dark23@TheBlack>
2022-12-19 20:36:39 -0300

    Java Web Git 01

---
+++ javaWebGit/src/javaWebGit/MainTesting.java
@@ -0,0 +1,11 @@
+package javaWebGit;
+
+public class MainTesting {
+
+	public static void main(String[] args) {
+		
+		//First Java Project Git.
+		System.out.println("Hi Devil's Git!");
+		System.out.println("Hi Again!");
+	}
+}
